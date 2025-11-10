43d2d23895d22c7537231cb66f9bd3f92862ed69
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2025-11-10 15:52:41 -0500

    Update NVIDIA installation script with extrepo support

--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -10,12 +10,22 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-wget https://developer.download.nvidia.com/compute/cuda/13.0.2/local_installers/cuda-repo-debian12-13-0-local_13.0.2-580.95.05-1_amd64.deb
-sudo dpkg -i cuda-repo-debian12-13-0-local_13.0.2-580.95.05-1_amd64.deb
-sudo cp /var/cuda-repo-debian12-13-0-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo apt-get update
-sudo apt-get -y install cuda-toolkit-13-0
-sudo apt-get -y install nvidia-driver
+
+sudo apt install extrepo -y
+extrepo update
+sudo extrepo enable nvidia-cuda
+sudo apt update
+sudo apt install build-essential linux-headers-amd64 -y
+sudo apt install nvidia-open-580 -y
+
+
+
+#wget https://developer.download.nvidia.com/compute/cuda/13.0.2/local_installers/cuda-repo-debian12-13-0-local_13.0.2-580.95.05-1_amd64.deb
+#sudo dpkg -i cuda-repo-debian12-13-0-local_13.0.2-580.95.05-1_amd64.deb
+#sudo cp /var/cuda-repo-debian12-13-0-local/cuda-*-keyring.gpg /usr/share/keyrings/
+#sudo apt-get update
+#sudo apt-get -y install cuda-toolkit-13-0
+#sudo apt-get -y install nvidia-driver
 
 
 echo "Rebooting to apply changes..."
