afa01c766340a65af3f0ab59fcc8b45dbbbbbf7d
PiercingXX <155384428+PiercingXX@users.noreply.github.com>
2026-01-12 15:06:52 -0500

    Update debian-server.sh

--- debian-server.sh
+++ debian-server.sh
@@ -418,7 +418,6 @@ EOF
     sudo apt update -y
     sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
     sudo usermod -aG docker "$USERNAME"
-    newgrp docker
 
 # Docker Daemon Configuration
     echo -e "${YELLOW}Configuring Docker daemon…${NC}"
